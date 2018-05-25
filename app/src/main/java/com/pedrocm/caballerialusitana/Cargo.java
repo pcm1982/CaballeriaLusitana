e572b887ef857e3189d1d87c72a8444dfda65f72
Pedro Camara <pcm1982@gmail.com>
2018-05-25 15:15:10 +0200

    Initial commit

---
+++ app/src/main/java/com/pedrocm/caballerialusitana/Cargo.java
@@ -0,0 +1,11 @@
+package com.pedrocm.caballerialusitana;
+
+/**
+ * Created by PedroCM on 25/05/2018.
+ */
+
+public enum Cargo {
+
+    TROPA,VOCAL,TESORERO,SECRETARIO,VICEPRESIDENTE,PRESIDENTE
+
+}
---
+++ app/src/main/java/com/pedrocm/caballerialusitana/Usuario.java
@@ -0,0 +1,64 @@
+package com.pedrocm.caballerialusitana;
+
+/**
+ * Created by PedroCM on 25/05/2018.
+ */
+
+public class Usuario {
+    private int id;
+    private String username;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Usuario usuario = (Usuario) o;
+
+        if (id != usuario.id) return false;
+        if (!username.equals(usuario.username)) return false;
+        return password.equals(usuario.password);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + username.hashCode();
+        result = 31 * result + password.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
